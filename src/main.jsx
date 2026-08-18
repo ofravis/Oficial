178e0e6b1c3bfc4f9b93571272e19d7c9ef92182
ofravis <flavio.a.azevedo6@aluno.senai.br>
2026-08-18 08:55:50 -0300

    index erro corrigido se pá

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./Index.css";
+import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
